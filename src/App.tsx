189224e8abf125050dcc02393ae4325e78822b36
muhammadderic <muhammad.donny.e@gmail.com>
2024-05-25 13:25:51 +0700

    Setup project

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <>
+      <h1 className="p-10">Hello Deric</h1>
+      <h1 className="text-xl">Hello Deric</h1>
+    </>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+import { join } from "path";
+
+export default {
+  // eslint-disable-next-line no-undef
+  content: [join(__dirname, 'src/**/*.{js,ts,jsx,tsx}')],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
